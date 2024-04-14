859e04c8caebccf6c8f010e2378d85439fb50522
Daniel <thedanils63@mail.ru>
2024-04-14 20:51:44 +0300

    add new fields in userDto and components in frontend

--- frontend/video-hosting-ui/src/app/app.module.ts
+++ frontend/video-hosting-ui/src/app/app.module.ts
@@ -28,6 +28,11 @@ import {VideoPlayerComponent} from './video-player/video-player.component';
 import { AuthConfigModule } from './auth/auth-config.module';
 import {AuthInterceptor} from "angular-auth-oidc-client";
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { LikedVideosComponent } from './liked-videos/liked-videos.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
     declarations: [
@@ -36,7 +41,12 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
         HeaderComponent,
         SaveVideoDetailsComponent,
         VideoPlayerComponent,
-        VideoDetailComponent
+        VideoDetailComponent,
+        HomeComponent,
+        HistoryComponent,
+        SubscriptionsComponent,
+        LikedVideosComponent,
+        SidebarComponent
     ],
     imports: [
         BrowserModule,
--- user-service/src/main/java/ru/simankovd/userservice/dto/UserDto.java
+++ user-service/src/main/java/ru/simankovd/userservice/dto/UserDto.java
@@ -21,53 +21,23 @@ public class UserDto {
     private String lastName;
     private String fullName;
     private String emailAddress;
-    private String sub;
     private Set<String> subscribedToUsers = ConcurrentHashMap.newKeySet();
     private Set<String> subscribers = ConcurrentHashMap.newKeySet();
     private Set<String> videoHistory  = ConcurrentHashMap.newKeySet();
     private Set<String> likedVideos  = ConcurrentHashMap.newKeySet();
     private Set<String> dislikedVideos = ConcurrentHashMap.newKeySet();
 
-    public void addToLikeVideos(String videoId){
-        likedVideos.add(videoId);
-    }
-
-    public void removeFromLikedVideos(String videoId){
-        likedVideos.remove(videoId);
-    }
-
-    public void addToDislikedVideos(String videoId){
-        dislikedVideos.add(videoId);
-    }
-
-    public void removeFromDislikedVideos(String videoId){
-        dislikedVideos.remove(videoId);
-    }
-
-    public void addToVideoHistory(String videoId){
-        videoHistory.add(videoId);
-    }
-
-    public void addToSubscribedTOUsers(String userId){
-        subscribedToUsers.add(userId);
-    }
-
-    public void addToSubscribers(String userId){
-        subscribers.add(userId);
-    }
-
-    public void removeFromSubscribedToUsers(String userId){
-        subscribedToUsers.remove(userId);
-    }
-
-    public void removeFromSubscribers(String userId){
-        subscribers.remove(userId);
-    }
-
-    public static UserDto from(User user){ // todo добавить остальные поля
+    public static UserDto from(User user){
         return UserDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .fullName(user.getFullName())
+                .emailAddress(user.getEmailAddress())
                 .likedVideos(user.getLikedVideos())
                 .dislikedVideos(user.getDislikedVideos())
+                .videoHistory(user.getVideoHistory())
+                .subscribedToUsers(user.getSubscribedToUsers())
+                .subscribers(user.getSubscribers())
                 .build();
     }
 
